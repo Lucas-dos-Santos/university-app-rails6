a6e7510002114c918a917a03b62db7fa13e57987
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-21 16:00:57 -0300

    create authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
-
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :students
 end

e89ea3d10ab0dd970fd4be9c802f026d4fb14486
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-21 19:24:26 -0300

    add auth methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    student = Student.find_by(email: params[:sessions][:email])
+    if student && student.authenticate(params[:sessions][:password])
+      session[:student_id] = student.id
+      flash[:notice] = 'Estudante logado com sucesso'
+      redirect_to student
+    else
+      flash.now[:notice] = 'Login ou senha inválidos!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:student_id] = nil
+    flash[:notice] = 'Deslogado com sucesso!'
+    redirect_to root_path
+  end
 end
